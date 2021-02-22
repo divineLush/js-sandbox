21e5a2f8218dcec89de7f2c80c244c6476d710f2
divineLush <homeoflucifer@gmail.com>
2021-02-22 04:57:17 -0500

    primitive types

---
+++ js/types-and-operators/primitive-types.js
@@ -0,0 +1,20 @@
+// DYNAMIC TYPING
+
+var foo = true;
+foo = 1;
+foo = 'abc';
+
+console.log(foo);
+
+
+// PRIMITIVE TYPES
+
+// Primitive type is a type of data that represents a single value (not an object)
+
+// 1. undefined (represents lack of existence, shouldn't be set manually)
+// 2. null (represents lack of existence, should be set manually)
+// 3. boolean
+// 4. number (floating point number)
+// 5. string (sequence of characters, both '' and "" can be used)
+// 6. symbol (used in ES6)
+
