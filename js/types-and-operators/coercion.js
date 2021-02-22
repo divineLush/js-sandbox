e4bf1678a3683ff3ebd01aa95f5b2a40344675fc
divineLush <homeoflucifer@gmail.com>
2021-02-22 08:32:42 -0500

    coercion

---
+++ js/types-and-operators/coercion.js
@@ -0,0 +1,19 @@
+// COERCION
+
+// converting a value from one type to another
+
+// 3
+console.log(1 + 2);
+
+// Hello World!
+console.log('Hello' + 'World!');
+
+// 12 string
+var a = 1 + '2';
+console.log(a, typeof a);
+
+// 1abc
+console.log(1 + 'abc');
+
+// NaN
+console.log(1 - 'abc');
