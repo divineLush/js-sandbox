9399287f701790d61b2847edc81fd6d7ad65fa78
divineLush <homeoflucifer@gmail.com>
2021-02-26 23:12:05 +0700

    prototypes

---
+++ js/object-oriented-js/prototypes.js
@@ -0,0 +1,33 @@
+// PROTOTYPE
+
+var person = {
+    firstName: 'default',
+    lastName: 'default',
+    getFullName: function () {
+        return this.firstName + ' ' + this.lastName;
+    },
+};
+
+var john = {
+    firstName: 'John',
+    lastName: 'Doe',
+};
+
+// DON'T DO THIS EVER
+// PERFORMANCE ISSUE
+john.__proto__ = person;
+// john now inherits from person
+// John Doe
+console.log(john.getFullName());
+// John Doe
+// won't return default default because of prototype chain
+// JS engine finds firstName inside john and stops
+console.log(john.firstName, john.lastName);
+
+var jane = {
+    firstName: 'Jane',
+};
+
+jane.__proto__ = person;
+// Jane default
+console.log(jane.getFullName());
